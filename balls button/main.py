c8a6516ef93f2561dae897aa222a1c404d6038da
dubstepfuneral <chillytp@outlook.com>
2021-07-29 14:53:34 +0500

    it prints it now LOL

--- balls button/main.py
+++ balls button/main.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from pygame import mixer
 import os
 from random import randrange
+from time import sleep
 
 path = os.path.dirname(os.path.abspath(__file__))
 path = path + "\\resources"
@@ -14,6 +15,44 @@ def play():
     mixer.music.load(path + "\\" + audioFile)
     mixer.music.set_volume(0.3)
     mixer.music.play()
+    os.system('cls')
+    sleep(0.6)
+    print('CAN I')
+    sleep(0.4)
+    print('PUT MY BALLS')
+    sleep(1.2)
+    print('IN YOUR JAW')
+    sleep(1.2)
+    print('(YOUR JAAW!)')
+    sleep(1)
+    print('BALLS IN YO JAW?')
+    sleep(2)
+    print('CAN I')
+    sleep(1.7)
+    print('CAN I?')
+    sleep(1.6)
+    print('(CAN I)')
+    sleep(1.4)
+    print('CAN I?')
+
+    sleep(1.6)
+    print('CAN I')
+    sleep(0.4)
+    print('PUT MY BALLS')
+    sleep(1.1)
+    print('IN YOUR JAW')
+    sleep(1.2)
+    print('(YOUR JAAW!)')
+    sleep(1)
+    print('BALLS IN YO JAW?')
+    sleep(2)
+    print('CAN I')
+    sleep(1.6)
+    print('CAN I?')
+    sleep(1.6)
+    print('(CAN I)')
+    sleep(1.4)
+    print('CAN I?')
 
 root = Tk()
 root.title("balls")
