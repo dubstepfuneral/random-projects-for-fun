803eb13573a288f49d244570d8bea5608a89cc0d
dubstepfuneral <chillytp@outlook.com>
2021-07-29 11:44:10 +0500

    i made a button that plays the random audiofile from the resources folder and the audio fies are supposed to be popular ppl saying balls but i didnt find more except the song

---
+++ balls button/main.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+from pygame import mixer
+import os
+from random import randrange
+
+path = os.path.dirname(os.path.abspath(__file__))
+path = path + "\\resources"
+audioFile = os.listdir(path)[randrange(0, len(os.listdir(path)))]
+mixer.init()
+
+def play():
+    global path, audioFile
+    mixer.music.load(path + "\\" + audioFile)
+    mixer.music.set_volume(0.3)
+    mixer.music.play()    
+
+root = Tk()
+root.title("balls")
+ttk.Button(root, text="Balls!", command=play, width=20).grid(row=1, column=0)
+root.mainloop()
--- balls.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-
-root = Tk()
-root.title("Balls")
-
-#thats all im gonna do for today 27.07.2021 omegalul
-root.mainloop()
